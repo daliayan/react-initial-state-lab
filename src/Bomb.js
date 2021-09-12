6d3a5ed66f18ac16ee2e38616bff22609c8373e7
daliayan <daliayan5@gmail.com>
2021-09-12 18:54:53 -0400

    React component states - states vs props

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+
+    constructor(props){
+        super();
+        this.state = {
+            secondsLeft: props.initialCount,
+        };
+    }
+
+    render(){
+        const saying = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+        return <div>{saying}</div>
+    }
+
+
+}
+export default Bomb;
